ee22c037e9301d9888a21620fefaf158b93cbde9
Вадим Лосенков <losenkov.dev6@gmail.com>
2023-09-30 15:47:18 +0300

    finished adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -75,6 +75,37 @@ if (checkPage('home-page')) {
 } else if (checkPage('news-page')) {
   new Aside(false);
 } else if (checkPage('ranking-page')) {
+  const $backdrop = document.querySelector('[data-filter="backdrop"]');
+  const $wrapper = document.querySelector('[data-filter="wrapper"]');
+  const $button = document.querySelector('[data-filter="button"]');
+
+  const openFilter = () => {
+    $backdrop.style.display = 'block';
+    setTimeout(() => {
+      $backdrop.classList.add('opened');
+    }, 10);
+
+    $wrapper.classList.add('opened');
+    document.body.classList.add('popup-opened');
+  };
+
+  const closeFilter = () => {
+    $backdrop.classList.remove('opened');
+    setTimeout(() => {
+      $backdrop.style.display = 'none';
+    }, 300);
+
+    $wrapper.classList.remove('opened');
+    document.body.classList.remove('popup-opened');
+  };
+
+  $button.addEventListener('click', openFilter);
+  $backdrop.addEventListener('click', closeFilter);
+
+  $wrapper.addEventListener('click', (e) => {
+    e.target.closest('[data-filter="close"]') && closeFilter();
+  });
+
   new Aside(false);
 } else if (checkPage('article-page')) {
   new Aside(true);
